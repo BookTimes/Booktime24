3b1b943f7863eb40e6721c270a3401357a4732cf
Mahadev Maneesh <maha040963@sisharjah.com>
2024-05-02 16:25:30 +0400

    Added time table

--- script.js
+++ script.js
@@ -40,10 +40,88 @@ updateDate();
 let functog = true;
 document.querySelector(".func").addEventListener("click", function () {
   if (functog == true) {
+    document.querySelector("#scheduleContainer").classList.add("hid");
     document.querySelector(".ell").classList.remove("hid");
+
     functog = false;
   } else {
     document.querySelector(".ell").classList.add("hid");
+    document.querySelector("#scheduleContainer").classList.remove("hid");
+
     functog = true;
   }
 });
+
+function getCurrentActivity(schedule) {
+  // Get current time
+  var currentTime = new Date();
+  var currentHour = currentTime.getHours();
+  var currentMinute = currentTime.getMinutes();
+  var currentFormattedTime =
+    currentHour + ":" + (currentMinute < 10 ? "0" : "") + currentMinute;
+
+  for (var activity in schedule) {
+    if (schedule.hasOwnProperty(activity)) {
+      var activityTime = schedule[activity];
+
+      if (currentFormattedTime >= activityTime) {
+        var nextActivity = getNextActivity(schedule, activityTime);
+
+        if (currentFormattedTime < nextActivity.time) {
+          return activity;
+        }
+      }
+    }
+  }
+  return null;
+}
+
+function getNextActivity(schedule, currentTime) {
+  for (var activity in schedule) {
+    if (schedule.hasOwnProperty(activity)) {
+      var activityTime = schedule[activity];
+
+      if (activityTime > currentTime) {
+        return {
+          activity: activity,
+          time: activityTime,
+        };
+      }
+    }
+  }
+  return null;
+}
+
+var schedule = {
+  Attendance: "7:00",
+  "1st Period": "7:15",
+  "2nd Period": "7:55",
+  Break1: "8:35",
+  "3rd Period": "8:50",
+  "4th Period": "9:25",
+  "5th Period": "10:00",
+  "6th Period": "10:35",
+  "7th Period": "11:10",
+  "8th Period": "11:45",
+  "9th Period": "12:20",
+  Break2: "12:55",
+  "10th Period": "1:15",
+  Dispersal: "1:50",
+};
+
+setInterval(() => {
+  var currentActivity = getCurrentActivity(schedule);
+  var spans = document
+    .getElementById("scheduleContainer")
+    .getElementsByTagName("span");
+  for (var i = 0; i < spans.length; i++) {
+    var spanTime = spans[i].textContent;
+    if (spanTime === currentActivity) {
+      spans[i].classList.add("pgd");
+    } else {
+      if ("pgd" in spans[i].classList) {
+        spans[i].classList.remove("pgd");
+      }
+    }
+  }
+}, 6000);
